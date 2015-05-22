0977c89edcc5eaded23e4283349512b822396a4a
squarefk <1497272646@qq.com>
2015-05-22 10:29:53 +0800

    first period

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			gameObject.GetComponent<Transform> ().Translate (Vector3.forward * Time.deltaTime * 5);
+		if (Input.GetKey (KeyCode.S))
+			gameObject.GetComponent<Transform> ().Translate (Vector3.back * Time.deltaTime * 5);
+		if (Input.GetKey (KeyCode.A))
+			gameObject.GetComponent<Transform> ().Rotate (0, -Time.deltaTime * 90, 0);
+		if (Input.GetKey (KeyCode.D))
+			gameObject.GetComponent<Transform> ().Rotate (0, Time.deltaTime * 90, 0);
+		if (Input.GetKey (KeyCode.Space))
+			gameObject.GetComponent<Transform> ().Translate(Vector3.up * Time.deltaTime * 3);
+
+		if (Input.GetKey (KeyCode.Space))
+			gameObject.GetComponent<Animation> ().Play("dragon_take_off");
+		else if (Input.GetKey (KeyCode.W))
+			gameObject.GetComponent<Animation> ().Play ("dragon_stirr_on_ground");
+		else if (Input.GetKey (KeyCode.S))
+			gameObject.GetComponent<Animation> ().Play ("dragon_stirr_on_ground");
+		else if (Input.GetKey (KeyCode.A))
+			gameObject.GetComponent<Animation> ().Play ("dragon_stirr_on_ground");
+		else if (Input.GetKey (KeyCode.D))
+				gameObject.GetComponent<Animation> ().Play ("dragon_stirr_on_ground");
+		else
+			gameObject.GetComponent<Animation> ().Play ("dragon_idle");
+	}
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+	// Use this for initialization
+	public GameObject sample;
+
+	void Start () {
+		int mp[110][110];
+		int width = 100, height = 100;
+
+		//make walls around
+		for (int i=1; i<=width; ++i) {
+			mp[0][i]=-1;
+			mp[height][i]=-1;
+		}
+		for (int i=1; i<=height; ++i) {
+			mp[i][0]=-1;
+			mp[i][width]=-1;
+		}
+
+		Random(100);
+
+		/*
+		for (int i=-5; i<=5; ++i)
+			for (int j=-5; j<=5; ++j)
+				if (j%3==0)
+					GameObject.Instantiate (sample,
+					                        new Vector3(100+i*2, 1, 100+j*2),
+				             	 			Quaternion.identity);
+		GameObject.Destroy (sample);
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
